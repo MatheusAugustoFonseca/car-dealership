3338081d55b48f3593086bedcf3adc49a5791a0e
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-04-17 16:22:58 -0300

    adding tests on MotorcycleService after saving

---
+++ tests/unit/Mocks/getAllMotorcycle.mock.ts
@@ -0,0 +1,34 @@
+const getAllMotorcycle = [
+  {
+    id: '643d993ca0a3b50147987c57',
+    model: 'biiiiiiiiiz',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '643d9a86a0a3b50147987c5a',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '643d9ad7a0a3b50147987c5c',
+    model: 'CG',
+    year: 2020,
+    color: 'Yellow',
+    status: true,
+    buyValue: 20,
+    category: 'Street',
+    engineCapacity: 400,
+  },
+];
+
+export default getAllMotorcycle;
